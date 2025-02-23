c48964e82001a18df3d03d297eca131c8cd10331
sanjana <youremail@example.com>
2025-02-23 20:39:16 +0530

    some code

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Add from "./components/Add";
+
+// import Greet from "./components/Greeting";
+
+
+// import Header from "./components/Header";
+// import MainCont from "./components/MainCont";
+// import Footer from "./components/Footer";
+// import WelcomeMsg from "./components/WelcomeMsg";
+import JsxRules from "./components/JsxRules";
+
+const App = () => {
+  return  (
+           <div>
+           {
+           /* <Header />
+           <MainCont />
+           <Footer /> */
+
+          //  <WelcomeMsg />
+
+           <JsxRules />
+
+          }
+           </div>
+
+           );
+  };
+  export default App;
+
+  // const Preptext = () => {
+  // return <div >this is some text over There</div>;
+  // };
+  // export default Preptext;
---
+++ src/components/WelcomeMsg.jsx
@@ -0,0 +1,17 @@
+const WelcomeMsg = () => {
+    return (
+    
+    <section id="section">
+       <h1 className="my-class">Welcome Form</h1>
+       <form>
+        <label htmlFor="name">Name </label>
+        <input type="text" placeholder="Enter Your Name" />
+       </form>
+
+    </section>
+    
+    );
+    
+   };
+   
+   export default WelcomeMsg;
