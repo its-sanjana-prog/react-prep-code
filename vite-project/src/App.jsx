16e5a00d365593a59488555efeb2c0e75eb09200
sanjana <youremail@example.com>
2025-03-17 20:21:24 +0530

    Where is use of hooks and compleate the aad and remove assignment code

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,55 @@
 
-import Chai from "./Chai";
+import { useState } from "react";
 
 function App() {
+
+  let [counter, setCounter] = useState(5);
+
+  // counter is variable and setCounter is method/function who is responsible for the update the value of counter
+
+  // let counter = 5;
+
+  const addValue = () => {
+  // console.log('Value Added', Math.random());
+  if ( counter < 20) {
+
+  counter = counter + 1;
+  console.log('Value Added', counter);
+  setCounter(counter);
+ 
+  console.log('Element', counter);
+  
+   }else{
+    console.log('ADD NHI BUS AB');
+   }
+  }
+
+const  removeValue =()=>{
+
+  if (counter > 0) {
+
+  counter = counter - 1;
+  console.log('Value Removed', counter);
+  setCounter(counter);
+
+  console.log('Element', counter);
+  
+}else{
+  console.log('REMOVE NHI BUS AB');
+ }
+}
 
   return (
    
      <>
-      <Chai />
-        <h1>Start with vite</h1>
+      
+      <h1>Chai or react</h1>
+      <h2>Counter Vlaue : {counter}</h2>
+      <button onClick={addValue}>Add Value</button> <br />
+      <button onClick={removeValue}>Remove Value</button>
+
       </>
   )
 }
 
-export default App
+export default App 
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,41 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
  
-
-
-function MyApp() {
-  
-  return(
-    <>
-    <p>This is my app ka custom app</p>
-    </>
-  );
-}
-// This is not working because there is not right syntax for this
-// const ReactElement = {  
-//   type : 'a', 
-//   props : {
-//     href : 'https://www.google.com',
-//     target : '_blank'
-//   } ,
-//   children : 'Click here'
-// };
-
-const text = 'chai or code';
-const anotherReactElement = (
-     <a href="https://www.google.com/" target='_blank'>Visit google </a>
-);
-
-// const ReactElement = React.createElement('a', {href: 'https://www.google.com', target: '_blank'},
-//                             'Search on google',{text}
-// );
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    {/* <MyApp /> */}
-    {/* {ReactElement}
-    {anotherReactElement} */}
+
   </StrictMode>
 );
