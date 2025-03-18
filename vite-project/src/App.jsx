3ca2149b8cc958588fca5e5b1c8651d6c8214813
sanjana <youremail@example.com>
2025-03-18 15:45:00 +0530

    Tailwind Use in Prep  React+vite

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,55 +1,9 @@
-
-import { useState } from "react";
-
 function App() {
-
-  let [counter, setCounter] = useState(5);
-
-  // counter is variable and setCounter is method/function who is responsible for the update the value of counter
-
-  // let counter = 5;
-
-  const addValue = () => {
-  // console.log('Value Added', Math.random());
-  if ( counter < 20) {
-
-  counter = counter + 1;
-  console.log('Value Added', counter);
-  setCounter(counter);
- 
-  console.log('Element', counter);
-  
-   }else{
-    console.log('ADD NHI BUS AB');
-   }
-  }
-
-const  removeValue =()=>{
-
-  if (counter > 0) {
-
-  counter = counter - 1;
-  console.log('Value Removed', counter);
-  setCounter(counter);
-
-  console.log('Element', counter);
-  
-}else{
-  console.log('REMOVE NHI BUS AB');
- }
-}
-
   return (
-   
-     <>
-      
-      <h1>Chai or react</h1>
-      <h2>Counter Vlaue : {counter}</h2>
-      <button onClick={addValue}>Add Value</button> <br />
-      <button onClick={removeValue}>Remove Value</button>
-
-      </>
-  )
+    <div className="flex items-center justify-center h-screen bg-blue-500 text-white">
+      <h1 className="text-3xl font-bold underline">Hello Tailwind with React!</h1>
+    </div>
+  );
 }
 
-export default App 
+export default App;
